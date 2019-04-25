54cbf07af69fff451b6abea60b43df767c5bd3eb
Avi Menon <avimenon@Avis-MacBook-Pro.local>
2019-04-24 22:59:28 -0700

    client to server test

--- test/socket.test.js
+++ test/socket.test.js
@@ -3,48 +3,113 @@ var io = require('socket.io-client')
 
 assert = require('assert');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27018/";
+
+
 describe('basic socket.io example', function() {
 
-  var socket;
+    var socket;
 
-  beforeEach(function(done) {
-    // Setup
-    socket = io.connect('http://localhost:3001', {
-  
+    beforeAll(function(done){
+        done();
+    })
 
+    beforeEach(function(done) {
+        // Setup
+        socket = io.connect('http://localhost:3001', {
+        'reconnection delay' : 0
+        , 'reopen delay' : 0
+        , 'force new connection' : true
+        , transports: ['websocket']
+        });
+
+        socket.on('connect', () => {
+            done();
+        });
+
+        socket.on('disconnect', () => {
+        // console.log('disconnected...');
+        });
     });
 
-    socket.on('connect', () => {
-      done();
+    afterEach((done) => {
+        // Cleanup
+        if(socket.connected) {
+        socket.disconnect();
+        }
+        //io_server.close();
+        done();
     });
 
-    socket.on('disconnect', () => {
-      // console.log('disconnected...');
-    });
-  });
+    afterAll((done) => {
+        io_server.close();
+        done();
+    })
 
-  afterEach((done) => {
-    // Cleanup
-    if(socket.connected) {
-      socket.disconnect();
-    }
-    io_server.close();
-    done();
-  });
 
-  it('should communicate', (done) => {
-    // once connected, emit Hello World
-    io_server.emit('echo', 'Hello World');
+    it('should communicate', (done) => {
+        // once connected, emit Hello World
+        io_server.emit('echo', 'Hello World');
 
-    socket.once('echo', (message) => {
-      // Check that the message matches
-      assert(message === "Hello World");
-      done();
+        socket.once('echo', (message) => {
+            // Check that the message matches
+            assert(message === "Hello World");
+            done();
+        });
+
+        io_server.on('connection', (client) => {
+            assert(client != null);
+            console.log('we got a connection');
+        });
     });
 
-    io_server.on('connection', (socket) => {
-      assert(socket != null);
+    it('should be able to add a new user', (done) =>{
+
+        socket.emit('new-user',{
+            name      : "test_name",
+            roomID    : "test_roomid",
+            sessionID : "test_sessionid"
+        });
+
+
+        setTimeout(() => {
+            
+            io_server.on('connection', (client) => {
+
+                console.log('we got a connection');
+                assert(client != null);
+    
+                client.once('new-user',(data)=>{
+                    //name and roomID
+                    console.log('got request');
+                    client.join(data.roomID);
+                    client.sessionID = data.sessionID;
+            
+                    var newuser = {
+                        username   : data.name,
+                        roomID     : data.roomID,
+                        sessionID  :  data.sessionID
+                    };
+                    
+                    assert(username === "test_nam");
+                    // mongo.connect(mongourl, function(err, client){
+                    //     assert.equal(null,err);
+                    //     //db created
+                    //     var db = client.db('Colabor8Test');
+                    //     db.collection("Usernames").insertOne(newuser, function(err,result){
+                    //       assert.equal(null,err);
+                    //       console.log('item inserted, ',newuser);
+                    //       client.close();
+    
+                    //       done();
+                    //     });
+                    // });
+                });
+            });
+
+            done();
+          }, 500);
     });
-  });
 
 });
